655d4d30eb0b50b5a00a39c8a738234ed3e226e0
dorfir <greendorfir@gmail.com>
2025-04-11 17:45:45 +0200

    dev 1.0

---
+++ public/js/mock-produits.js
@@ -0,0 +1,50 @@
+const produits = [
+    {   
+        famille: "sanitaires",
+        categorie: "pare-bain",
+        marque: "Aurora NOVELLI",
+        nom : "Pare-bain droit",
+        descriptif : `
+            Verre traité anticalcaire,<br>
+            Epaisseur 6mm.<br>
+            <br>
+            Pivotant à 180°.<br>
+        `,
+        prix : null,
+        images : ["img/produits/pareBain_aurora.jpg"],
+    },
+
+    {   
+        famille: "sanitaires",
+        categorie: "receveurs",
+        marque: "Stepin SANINDUSA",
+        nom : "Receveur de douche céramique antidérapant",
+        descriptif : `
+            Dimensions disponibles:<br>
+            <ul>
+                <li>70x120/140 cm</li>
+                <li>80x80/100/120/140 cm</li>
+                <li>90x90/120/140 cm</li>
+            </ul>
+        `,
+        prix : null,
+        images : ["img/produits/receveurs_stepinSanindusa.jpg"],
+    },
+    {   
+        famille: "sanitaires",
+        categorie: "receveurs",
+        marque: "Flight JACOB DELAFON",
+        nom : "Receveur de douche acrylique",
+        descriptif : `
+            Dimensions disponibles:<br>
+            <ul>
+                <li>70x170 cm</li>
+                <li>76x170 cm</li>
+                <li>90x170 cm</li>
+            </ul>
+        `,
+        prix : null,
+        images : ["img/produits/receveurs_flightJacobDelafon.jpg"],
+    },
+
+]
