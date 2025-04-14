6e550617e43a42504d3b129d331e1803f9ecd074
dorfir <greendorfir@gmail.com>
2025-04-14 17:41:34 +0200

    dev 1.4

--- public/js/index.js
+++ public/js/index.js
@@ -54,6 +54,8 @@ function generateFiche(data) {
 }
 
 /* #region Produits */
+
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
@@ -76,43 +78,187 @@ const swiper = new Swiper('.swiper', {
     },
 });
 
-let lightBoxContainer = document.getElementById('lightbox-container')
-let popupProduitListe = document.getElementById('popup-produits-liste')
-let popupProduitsFicheContainer = document.getElementById('popup-produits-fiche-container')
-let listeProduitsElements = document.querySelectorAll('.popup-produits-liste-element')
-let popupProduitsBtnRetour = document.getElementById('popup-produits-btn-back')
-let popupProduitsBtnClose = document.getElementById('popup-produits-btn-close')
 
-for (let element of listeProduitsElements) {
+let lienProduitPareBain = document.getElementById('pareBain')
+let lienProduitReceveurs = document.getElementById('receveurs')
+let lienProduitParoisDouche = document.getElementById('paroisDouche')
+let lienProduitPortesDouche = document.getElementById('portesDouche')
 
-    element.addEventListener('click', (e)=> {
-        let element_index = getElementClassIndex(e.currentTarget, 'popup-produits-liste-element')
-        swiper.slideTo(element_index, 0, null)
-        popupProduitListe.style.zIndex = 0
-        popupProduitListe.style.display = "none"
-        popupProduitsFicheContainer.style.zIndex = 1
-        popupProduitsFicheContainer.style.display = "block"
-        popupProduitsBtnRetour.style.visibility = "visible"
-    })
+lienProduitPareBain.addEventListener('click', (e) => {
+    changeFicheProduitPopup(produits_pareBains)
+    let lightBoxContainer = document.getElementById('lightbox-container')
+    lightBoxContainer.style.display = "block"
+    lightBoxContainer.style.display.zIndex = 20
+})
+lienProduitReceveurs.addEventListener('click', (e) => {
+    changeFicheProduitPopup(produits_receveurs)
+    let lightBoxContainer = document.getElementById('lightbox-container')
+    lightBoxContainer.style.display = "block"
+    lightBoxContainer.style.display.zIndex = 20
+})
+lienProduitParoisDouche.addEventListener('click', (e) => {
+    changeFicheProduitPopup(produits_paroisDouche)
+    let lightBoxContainer = document.getElementById('lightbox-container')
+    lightBoxContainer.style.display = "block"
+    lightBoxContainer.style.display.zIndex = 20
+})
+lienProduitPortesDouche.addEventListener('click', (e) => {
+    changeFicheProduitPopup(produits_portesDouche)
+    
+})
 
-}
 
-popupProduitsBtnRetour.addEventListener('click', (e) => {
 
+function changeFicheProduitPopup(produits) {
+    initFicheProduitsHeader(produits[0].categorie)
+    initFicheProduitsListe(produits)
+    initListeProduitsListeners()
+    changeFicheProduitSwiper(produits)
+
+    let lightBoxContainer = document.getElementById('lightbox-container')
+    lightBoxContainer.style.display = "block"
+    lightBoxContainer.style.display.zIndex = 20
+
+    let popupProduitListe = document.getElementById('popup-produits-liste')
+    let popupProduitsFicheContainer = document.getElementById('popup-produits-fiche-container')
+    let popupProduitsBtnRetour = document.getElementById('popup-produits-btn-back')
     popupProduitListe.style.zIndex = 1
     popupProduitListe.style.display = "flex"
     popupProduitsFicheContainer.style.zIndex = 0
     popupProduitsFicheContainer.style.display = "none"
-    
     popupProduitsBtnRetour.style.visibility = "hidden"
-})
+}
 
-popupProduitsBtnClose.addEventListener('click', (e) => {
-    lightBoxContainer.style.display = "none"
-    lightBoxContainer.style.display.zIndex = 0
-})
+function initFicheProduitsHeader(name) {
+    let produitCategorie = document.getElementById('produit-categorie')
+    produitCategorie.innerText = name
+}
 
+function initFicheProduitsListe(produits) {
+    let popupProduitsListe = document.getElementById('popup-produits-liste')
+    popupProduitsListe.innerHTML = ""
+    produits.forEach(produit => {
+        let popupProduitsListeElement = document.createElement('div')
+        popupProduitsListeElement.classList.add('popup-produits-liste-element')
+        let imageContainer = document.createElement('div')
+        imageContainer.classList.add('image-container')
+        let image = document.createElement('img')
+        image.setAttribute('src', produit.thumb)
+        let titre = document.createElement('div')
+        titre.classList.add('titre')
+        titre.innerText = produit.marque
 
+        imageContainer.appendChild(image)
+        popupProduitsListeElement.appendChild(imageContainer)
+        popupProduitsListeElement.appendChild(titre)
+
+        popupProduitsListe.appendChild(popupProduitsListeElement)
+    })
+}
+
+function changeFicheProduitSwiper(produits) {
+    let swiperWrapper = document.getElementById('swiper-wrapper')
+    swiperWrapper.innerHTML = ""
+    produits.forEach(produit => {
+        swiperWrapper.appendChild(createFicheProduit(produit))  
+    })
+    swiper.update()
+}
+
+function createFicheProduit(fiche) {
+
+    let swiperSlide = document.createElement('div')
+    swiperSlide.classList.add('swiper-slide')
+    let swiperSlideSubcontainer = document.createElement('div')
+    swiperSlideSubcontainer.classList.add('swiper-slide-subcontainer')
+    let ficheProduit = document.createElement('div')
+    ficheProduit.classList.add('fiche-produit')
+    let modalMain = document.createElement('div')
+    modalMain.classList.add('modal-main')
+
+    let produitMarque = document.createElement('div')
+    produitMarque.classList.add('produit-marque')
+    produitMarque.innerText = fiche.marque
+    let produitNom = document.createElement('div')
+    produitNom.classList.add('produit-nom')
+    produitNom.innerText = fiche.nom
+
+    modalMain.appendChild(produitMarque)
+    modalMain.appendChild(produitNom)
+    
+    let group_count = Math.max(fiche.descriptifs.length, fiche.images.length)
+    for (let i=0; i < group_count; i++) {
+        
+        let produitGroup = document.createElement('div')
+        produitGroup.classList.add('produit-group')
+
+        let produitDescriptif = null
+        if (fiche.descriptifs.length > i) {
+            produitDescriptif = document.createElement('div')
+            produitDescriptif.classList.add('produit-descriptif')
+            produitDescriptif.innerHTML = fiche.descriptifs[i]
+        }
+        
+        let produitPicture = null
+        if (fiche.images.length > i) {
+            produitPicture = document.createElement('img')
+            produitPicture.classList.add('produit-picture')
+            produitPicture.setAttribute('src', fiche.images[i])
+        }
+
+        if (i%2 == 0) {
+            if (produitDescriptif !== null) produitGroup.appendChild(produitDescriptif)
+            if (produitPicture !== null) produitGroup.appendChild(produitPicture)
+        } else {
+            if (produitPicture !== null) produitGroup.appendChild(produitPicture)
+            if (produitDescriptif !== null) produitGroup.appendChild(produitDescriptif)
+        }
+        
+        modalMain.appendChild(produitGroup)
+    }
+    
+    
+    ficheProduit.appendChild(modalMain)
+    swiperSlideSubcontainer.appendChild(ficheProduit)
+    swiperSlide.appendChild(swiperSlideSubcontainer)
+    
+    return swiperSlide
+}
+
+function initListeProduitsListeners() {
+    let lightBoxContainer = document.getElementById('lightbox-container')
+    let popupProduitListe = document.getElementById('popup-produits-liste')
+    let popupProduitsFicheContainer = document.getElementById('popup-produits-fiche-container')
+    let popupProduitsBtnRetour = document.getElementById('popup-produits-btn-back')
+    let popupProduitsBtnClose = document.getElementById('popup-produits-btn-close')
+    let listeProduitsElements = document.querySelectorAll('.popup-produits-liste-element')
+
+    for (let element of listeProduitsElements) {
+        element.addEventListener('click', (e)=> {
+            console.log('-- listener produit liste element')
+            let element_index = getElementClassIndex(e.currentTarget, 'popup-produits-liste-element')
+            swiper.slideTo(element_index, 0, null)
+            popupProduitListe.style.zIndex = 0
+            popupProduitListe.style.display = "none"
+            popupProduitsFicheContainer.style.zIndex = 1
+            popupProduitsFicheContainer.style.display = "block"
+            popupProduitsBtnRetour.style.visibility = "visible"
+        })
+    }
+
+    popupProduitsBtnRetour.addEventListener('click', (e) => {
+        popupProduitListe.style.zIndex = 1
+        popupProduitListe.style.display = "flex"
+        popupProduitsFicheContainer.style.zIndex = 0
+        popupProduitsFicheContainer.style.display = "none"
+        popupProduitsBtnRetour.style.visibility = "hidden"
+    })
+    
+    popupProduitsBtnClose.addEventListener('click', (e) => {
+        lightBoxContainer.style.display = "none"
+        lightBoxContainer.style.display.zIndex = 0
+    })
+}
 
 /* #endregion */
 
--- public/js/mock-produits.js
+++ public/js/mock-produits.js
@@ -1,50 +1,217 @@
-const produits = [
+const produits_pareBains = [
     {   
-        famille: "sanitaires",
-        categorie: "pare-bain",
+        famille: "Sanitaires",
+        categorie: "Pare-bain",
         marque: "Aurora NOVELLI",
         nom : "Pare-bain droit",
-        descriptif : `
+        descriptifs : [
+            `
             Verre traité anticalcaire,<br>
             Epaisseur 6mm.<br>
             <br>
             Pivotant à 180°.<br>
-        `,
+            `   
+        ],
         prix : null,
         images : ["img/produits/pareBain_aurora.jpg"],
+        thumb : "./img/produits/pareBain_aurora_thumb.jpg",
     },
+]
 
+const produits_receveurs = [
     {   
-        famille: "sanitaires",
-        categorie: "receveurs",
+        famille: "Sanitaires",
+        categorie: "Receveurs",
         marque: "Stepin SANINDUSA",
         nom : "Receveur de douche céramique antidérapant",
-        descriptif : `
+        descriptifs : [
+            `
             Dimensions disponibles:<br>
             <ul>
                 <li>70x120/140 cm</li>
                 <li>80x80/100/120/140 cm</li>
                 <li>90x90/120/140 cm</li>
             </ul>
-        `,
+            `,
+            `
+            Dimensions disponibles <b>2</b>:<br>
+            <ul>
+                <li>70x120/140 cm</li>
+                <li>80x80/100/120/140 cm</li>
+                <li>90x90/120/140 cm</li>
+            </ul>
+            `,
+        ],
         prix : null,
-        images : ["img/produits/receveurs_stepinSanindusa.jpg"],
+        images : ["img/produits/receveurs_stepinSanindusa.jpg", "img/produits/receveurs_stepinSanindusa.jpg"],
+        thumb : "./img/produits/receveurs_stepinSanindusa_thumb.jpg",
     },
     {   
         famille: "sanitaires",
         categorie: "receveurs",
         marque: "Flight JACOB DELAFON",
         nom : "Receveur de douche acrylique",
-        descriptif : `
+        descriptifs : [ 
+            `
             Dimensions disponibles:<br>
             <ul>
                 <li>70x170 cm</li>
                 <li>76x170 cm</li>
                 <li>90x170 cm</li>
             </ul>
-        `,
+            `,
+        ],
         prix : null,
         images : ["img/produits/receveurs_flightJacobDelafon.jpg"],
+        thumb : "./img/produits/receveurs_flightJacobDelafon_thumb.jpg",
     },
-
 ]
+
+const produits_paroisDouche = [
+    {   
+        famille: "Sanitaires",
+        categorie: "Parois de douche",
+        marque: "Concerto Walk ALTERNA",
+        nom : "Paroi de douche fixe",
+        descriptifs : [
+            `
+            Verre de sécurité transparent 6mm<br>
+            Traitement anticalcaire<br>
+            Profilé en aluminium anosidé résistant aux rayures<br>
+            Bras de fixation<br>
+            Hauteur 200cm.<br>
+            <br>
+            Dimensions disponibles :
+            <ul>
+                <li>70 cm</li>
+                <li>80 cm</li>
+                <li>90 cm</li>
+                <li>100 cm</li>
+                <li>120 cm</li>
+                <li>140 cm</li>
+            </ul>
+            `   
+        ],
+        prix : null,
+        images : ["img/produits/paroisDouche_concertoWalkAlterna.jpg"],
+        thumb : "./img/produits/paroisDouche_concertoWalkAlterna_thumb.jpg",
+    },
+    {   
+        famille: "sanitaires",
+        categorie: "Parois de douche",
+        marque: "Zephyros NOVELLINI",
+        nom : "Paroi de douche fixe",
+        descriptifs : [
+            `
+            Verre de sécurité transparent 6mm<br>
+            Traitement anticalcaire<br>
+            Profilé en aluminium anosidé résistant aux rayures<br>
+            Bras de fixation<br>
+            Hauteur 200cm.<br>
+            <br>
+            Dimensions disponibles :
+            <ul>
+                <li>70 cm</li>
+                <li>80 cm</li>
+                <li>90 cm</li>
+                <li>100 cm</li>
+                <li>120 cm</li>
+                <li>140 cm</li>
+            </ul>
+            `   
+        ],
+        prix : null,
+        images : ["img/produits/paroisDouche_zephyrosNovellini_01.jpg", "img/produits/paroisDouche_zephyrosNovellini_02.jpg"],
+        thumb : "./img/produits/paroisDouche_zephyrosNovellini_thumb.jpg",
+    },
+]
+
+const produits_portesDouche = [
+    {   
+        famille: "Sanitaires",
+        categorie: "Portes de douche",
+        marque: "Concerto ALTERNA",
+        nom : "Porte de douche coulissante",
+        descriptifs : [
+            `
+            <u>Version 2 vantaux :</u><br>
+            1 panneau fixe et un panneau coulissant<br>
+            Verre transparent 6mm<br>
+            Profilé en aluminium<br>
+            Hauteur 195cm<br>
+            Dimensions disponibles :<br>
+            <ul>
+                <li>120 cm</li>
+                <li>140 cm</li>
+            </ul>
+            `,
+            `
+            <u>Version 3 vantaux :</u><br>
+            1 panneau fixe et 2 panneaux coulissants<br>
+            Verre transparent 6mm<br>
+            Profilé en aluminium<br>
+            Hauteur 195cm<br>
+            Dimensions disponibles :<br>
+            <ul>
+                <li>90 cm</li>
+            </ul>
+            `  
+        ],
+        prix : null,
+        images : ["img/produits/portesDouche_concertoAlterna_01.jpg", "img/produits/portesDouche_concertoAlterna_02.jpg"],
+        thumb : "./img/produits/portesDouche_concertoAlterna_thumb.jpg",
+    },
+    {   
+        famille: "sanitaires",
+        categorie: "Portes de douche",
+        marque: "Domino ALTERNA",
+        nom : "Porte de douche pivotante",
+        descriptifs : [
+            `
+            Verre transparent securit 8mm<br>
+            Profilé en argent brillant, en aluminium anodisé résistant aux rayures<br>
+            Traité anti calcaire<br>
+            Pivot total intérieur et extérieur<br>
+            Fermeture par joint magnétique<br>
+            Hauteur 195 cm<br>
+            Dimensions disponibles :<br>
+            <ul>
+                <li>90 cm</li>
+                <li>120 cm (fixe 55 cm + pivot)</li>
+            </ul>
+            `,
+        ],
+        prix : null,
+        images : ["img/produits/portesDouche_dominoAlterna_01.jpg", "img/produits/portesDouche_dominoAlterna_02.jpg"],
+        thumb : "./img/produits/portesDouche_dominoAlterna_thumb.jpg",
+    },
+    {   
+        famille: "sanitaires",
+        categorie: "Portes de douche",
+        marque: "Concerto ALTERNA",
+        nom : "Porte de douche d'angle - retour fixe",
+        descriptifs : [
+            `
+            Verre transparent securit 8mm<br>
+            Porte à ouverture coulissante<br>
+            <br>
+            Profilé en argent brillant, en aluminium anodisé résistant aux rayures<br>
+            Traité anti calcaire<br>
+            Hauteur 195 cm<br>
+            <br>
+            Dimensions disponibles :<br>
+            <ul>
+                <li>70 cm</li>
+                <li>80 cm</li>
+                <li>90 cm</li>
+                <li>100 cm</li>
+            </ul>
+            `,
+        ],
+        prix : null,
+        images : ["img/produits/portesDouche_concertoAlternaAngle.jpg"],
+        thumb : "./img/produits/portesDouche_concertoAlternaAngle_thumb.jpg",
+    },
+]
+
+
