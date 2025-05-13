31643f9df2f6792d06074a2c97b13365d7050536
dorfir <greendorfir@gmail.com>
2025-05-13 17:56:44 +0200

    dev 2.1

--- public/backoffice/script.js
+++ public/backoffice/script.js
@@ -1,3 +1,5 @@
+displayLoader()
+
 var app = app || {}
 app.edit = app.edit || {}
 app.edit = {
@@ -33,6 +35,7 @@ let categoriesReceived = false
 let liste_familles = null
 let index_famille = null
 const eventListeFamilleReceived = new Event("event-liste-famille-received")
+const eventNewFamilleInserted = new Event("event-new-famille-inserted")
 
 // getAllProduits()
 async function getAllProduits() {
@@ -82,6 +85,9 @@ async function getListeFamilles() {
 
 window.addEventListener('event-liste-famille-received', (e)=> {
   initCreateProduct()
+  window.setTimeout(()=>{
+    hideLoader()
+  },400)
 }, false)
 
 
@@ -92,7 +98,6 @@ function initCreateProduct() {
   
 
 }
-
 
 function setSelectFamille() {
 
@@ -118,7 +123,7 @@ function setSelectFamille() {
   add_famille_btn.innerText = "+"
   create_famille_container.appendChild(add_famille_btn)
 
-  add_famille_btn.addEventListener('click', openCreateFamillePopup)
+  add_famille_btn.addEventListener('click', openEditFamillePopup)
 
   create_famille.appendChild(create_famille_container)
 }
@@ -140,25 +145,121 @@ function setSelectCategorie(id_famille) {
   return select_cat;
 }
 
-function openCreateFamillePopup() {
+function openEditFamillePopup() {
 
   let popup_container = document.getElementById('popup-container')
-
   popup_container.style.display = 'block'
   popup_container.style.zIndex = 20
-
   let popup_add_famille = document.getElementById('popup-add-famille')  
-  let popup_add_famille_close = document.getElementById('popup-add-famille-close')
 
-  popup_add_famille_close.addEventListener('click', closeCreateFamillePopup)
+  
+  let popup_add_famille_close = document.getElementById('popup-add-famille-close')
+  popup_add_famille_close.addEventListener('click', closeEditFamillePopup)
+
+
+  let add_famille_input = document.getElementById('add-famille-input')
+  add_famille_input.addEventListener('keyup', (e) => {
+    console.log(add_famille_input.value)
+    if (e.key === "Enter") { createNewFamille() }
+    if (add_famille_input.value.length > 2) { 
+      add_famille_input.style.border = "1px solid rgb(118,118,118)" 
+      add_famille_input.style.outlineColor = "rgb(118,118,118)"
+    } else {
+      add_famille_input.style.border = "1px solid rgb(255,0,0)"
+      add_famille_input.style.outlineColor = "rgb(255,0,0)"
+    }
+  })
+  let popup_add_famille_button = document.getElementById('add-famille-button')
+  popup_add_famille_button.addEventListener('click', createNewFamille)
 
 }
-function closeCreateFamillePopup() {
+function closeEditFamillePopup() {
   let popup_container = document.getElementById('popup-container')
 
   popup_container.style.display = 'none'
   popup_container.style.zIndex = -1
 }
 
+function createNewFamille() {
+  let add_famille_input = document.getElementById('add-famille-input')
+  let input_value = add_famille_input.value
+  if (input_value.length > 2) {
+    input_value = input_value.charAt(0).toUpperCase() + input_value.slice(1)
+    // console.log('-- create new famille : ' + input_value)
+    sendNewFamille(input_value)
+  }
+}
+
+/* Send new famille création */
+async function sendNewFamille(nom_famille) {
+  
+  let json = null
+  const url = "http://localhost/green_catalogue_rest/createFamille.php"
+  let formData = new FormData()
+  formData.append('nom_famille', nom_famille)
+  try {
+    const response = await fetch(url, {
+      method: "post",
+      body: formData,
+    });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    json = await response.json()
+    console.log(json)
+    if (json['status'] == 200) {
+      let inserted_id_famille = json['id_famille']
+      console.log(inserted_id_famille)
+      window.dispatchEvent(eventNewFamilleInserted)
+    }  
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+window.addEventListener('event-new-famille-inserted', newFamilleInserted, false)
+function newFamilleInserted() {
+  console.log('-- new famille inserted')
+  // todo popup message
+  closeEditFamillePopup()
+}
 
 
+/* Upload file */
+function srcToFile(src, fileName, mimeType){
+  return (fetch(src)
+      .then(function(res){return res.arrayBuffer();})
+      .then(function(buf){return new File([buf], fileName, {type:mimeType});})
+  );
+}
+
+function uploadImage() {
+  
+  const imagePath = './img/dragon2.jpg'
+  const image = new Image()
+  image.src = imagePath
+  let myImage = document.getElementById('image')
+  myImage.setAttribute('src', imagePath)
+
+  srcToFile('./img/dragon2.jpg', 'new.jpg', 'image/jpg')
+  .then(function(file){
+    var fd = new FormData();
+    fd.append('image', file);
+    return fetch('http://localhost/green_catalogue_rest/createImage.php', {method:'POST', body:fd});
+  })
+  .then(function(res){
+    return res.json();
+  })
+  .then(console.log)
+  .catch(console.error)
+}
+
+
+/* Loader */
+function displayLoader() {
+  let loaderContainer = document.getElementById('loader-container')
+  loaderContainer.style.display = "block"
+}
+function hideLoader() {
+  let loaderContainer = document.getElementById('loader-container')
+  loaderContainer.style.display = "none"
+}
